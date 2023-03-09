cae2244998f9859b1e6f8cb6a35104fb7bee4d11
morgankn <118085617+morgankn@users.noreply.github.com>
2023-03-09 17:59:52 -0500

    added files

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const config = require('./config/connection');
+const routes = require('./controllers');
+
+const app = express();
+const hbs = exphbs.create();
+const PORT = process.env.PORT || 3001;
+
+app.use(session(config.expressSessionConfig));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(routes);
+
+config.sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
